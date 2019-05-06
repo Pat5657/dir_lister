39795d676426ac008bf53aa68ce6401d8a05ea4b
Pat <pat@DESKTOP-R0KAHJH.localdomain>
2019-05-06 12:20:54 +0100

    Cleaned up the code and moved the CSS to a CSS folder

--- index.php
+++ index.php
@@ -1,61 +1,30 @@
 <head>
-<style>
-a:visited a:link a:active {
-	text-decoration: none;
-}
+	<!-- Title -->
+	<title>Local Web Dev</title>
 
-.container {
-	background-color:#eaeaea;
-	min-width:300px;
-	max-width:50%;
-	height:97%;
-	margin:auto;
-	padding:2px 10px 10px;
-}
-
-body {
-	background-color:#b4bac4;
-}
-
-h1 {
-	line-height:10px;
-	font-family:Bahnschrift;
-}
-
-.dir {
-	font-family:Bahnschrift Light;
-	color:#212121;
-}
-
-
-</style>
-
+	<!-- Resource links -->
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
-<title>Local Web Dev</title>
+
 <body>
-
-<div class="container">
-
-
-<?php
-
-// Page title
-echo "<h1>LOCAL WEB DEV</h1><hr>";
-
-//search current dir
-foreach (glob("*") as $filename) {
-	if ($filename != "index.php") {
-			//echo each file name in the dir and hyperlink it
-			echo "<a class='dir' href=$filename>$filename</a> <br>";
-	} else {
-		continue;
-	}
-}
-
-?>
-
-</div>
-
-
-
+	<main>
+		<header>
+			<h1>LOCAL WEB DEV</h1>
+			<hr>
+		</header>
+		
+		<!-- Main script -->
+		<?php
+			//search current dir
+			foreach (glob("*") as $filename) {
+				if ($filename != "index.php") {
+						//echo each file name in the dir and hyperlink it
+						echo "<a class='dir' href=$filename>$filename</a> <br>";
+				} else {
+					continue;
+				}
+			}
+		?>
+		<!-- Main script -->
+	<main>
 </body>
